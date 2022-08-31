fb73103f37dd7a38f2222d811486c30d0552bf7c
Konstantin <zarudk@amazon.com>
2022-08-31 17:25:16 +0200

    Udpdate code to have a symbols only. Tested locally

--- app/index.js
+++ app/index.js
@@ -5,12 +5,14 @@ import { HeartRateSensor } from 'heart-rate';
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
 
-// Update the clock every minute
-clock.granularity = "seconds";
+
 
 const myLabel = document.getElementById("appTime");
-const battery_level_element = document.getElementById('bat_level');
-const heart_rate_element = document.getElementById('heart_rate');
+const battery_level_element = document.getElementById("bat_level");
+const heart_rate_element = document.getElementById("heart_rate");
+
+const prgs1 = document.getElementById("prgs1");
+const prgs2 = document.getElementById("prgs2");
 
 // Hart rate sensor even handling
 if (HeartRateSensor) {
@@ -23,9 +25,12 @@ if (HeartRateSensor) {
     } else if (hrm.heartRate < 100) {
       heart_rate_element.text = hrm.heartRate + "❤️";
       heart_rate_element.style.fill = "white";
+    } else if (100 < hrm.heartRate < 150) {
+      heart_rate_element.text = hrm.heartRate + "❤️";
+      heart_rate_element.style.fill = "yellow";
     }
     else {
-      heart_rate_element.text = hrm.heartRate;
+      heart_rate_element.text = hrm.heartRate + "❤️";
       heart_rate_element.style.fill = "red";
     }
   });
@@ -34,7 +39,8 @@ if (HeartRateSensor) {
   console.log("This device does NOT have a HeartRateSensor!");
 }
 
-// Update the <text> element every tick with the current time
+// Update the clock every second
+clock.granularity = "seconds";
 clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
@@ -56,5 +62,6 @@ clock.ontick = (evt) => {
     battery_level_element.style.fill = "white";
   }
   //console.log(battery_level_element.text)
+  prgs1.sweepAngle = battery.chargeLevel;
 
 }
